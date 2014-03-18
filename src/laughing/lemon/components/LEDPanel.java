b2fbada29ce5c992cb2b0f98c469470670319ee9
LaughingLemon <gnome@gnome.com>
2014-03-18 16:39:56 +0000

    Renaming constants to upper case. Cater for minus (-1).

--- src/laughing/lemon/components/LEDPanel.java
+++ src/laughing/lemon/components/LEDPanel.java
@@ -14,18 +14,18 @@ public class LEDPanel extends Panel {
     }
 
     //setter (bean compatibility)
-    public void setDisplayNumber(int displayNumber) {
+    public void setDisplayNumber(int value) {
         //only update is the value has actually changed
-        if(this.displayNumber != displayNumber % 10) {
-            //modulo 10 to create single digit
-            this.displayNumber = displayNumber % 10;
+        if(this.displayNumber != value % 11) {
+            //modulo 11 to create single digit or blank (10)
+            this.displayNumber = value % 11;
             //repaint the component
             repaint();
         }
     }
 
-    //points array is a constant
-    private static final int[][] points = new int[6][2];
+    //POINTS array is a constant
+    private static final int[][] POINTS = new int[6][2];
 
     //simple utility function to set the point co-ordinates
     private void setPoint(int[] inputPoint, int xCoOrd, int yCoOrd) {
@@ -33,45 +33,45 @@ public class LEDPanel extends Panel {
         inputPoint[1] = yCoOrd;
     }
 
-    //create an array of points representing a hexagonal segment
+    //create an array of POINTS representing a hexagonal segment
     private void populateSegmentPoints(int originX,   int originY,
                                        int thickness, int length) {
         //test to see if it's vertical segment
         if(thickness >= length) {
             //see documentation for details of how plots are calculated
-            setPoint(points[0], length / 2,             0);
-            setPoint(points[1], thickness - length / 2, 0);
-            setPoint(points[2], thickness,              length / 2);
-            setPoint(points[3], thickness - length / 2, length);
-            setPoint(points[4], length / 2,             length);
-            setPoint(points[5], 0,                      length / 2);
+            setPoint(POINTS[0], length / 2,             0);
+            setPoint(POINTS[1], thickness - length / 2, 0);
+            setPoint(POINTS[2], thickness,              length / 2);
+            setPoint(POINTS[3], thickness - length / 2, length);
+            setPoint(POINTS[4], length / 2,             length);
+            setPoint(POINTS[5], 0,                      length / 2);
         } else {
             // or horizontal
-            setPoint(points[0], thickness / 2,          0);
-            setPoint(points[1], thickness,              thickness / 2);
-            setPoint(points[2], thickness,              length - thickness / 2);
-            setPoint(points[3], thickness / 2,          length);
-            setPoint(points[4], 0,                      length - thickness / 2);
-            setPoint(points[5], 0,                      thickness / 2);
+            setPoint(POINTS[0], thickness / 2,          0);
+            setPoint(POINTS[1], thickness,              thickness / 2);
+            setPoint(POINTS[2], thickness,              length - thickness / 2);
+            setPoint(POINTS[3], thickness / 2,          length);
+            setPoint(POINTS[4], 0,                      length - thickness / 2);
+            setPoint(POINTS[5], 0,                      thickness / 2);
         }
 
         //shift everything relative to the
         //origin point
-        for(int[] point : points){
+        for(int[] point : POINTS){
             point[0] += originX;
             point[1] += originY;
         }
     }
 
-    //creates a segment polygon from array of points
+    //creates a segment polygon from array of POINTS
     private GeneralPath createSegment() {
         //create a polygon using the GeneralPath class
-        GeneralPath segment = new GeneralPath(GeneralPath.WIND_EVEN_ODD, points.length);
+        GeneralPath segment = new GeneralPath(GeneralPath.WIND_EVEN_ODD, POINTS.length);
         //start at the beginning...
-        segment.moveTo(points[0][0], points[0][1]);
+        segment.moveTo(POINTS[0][0], POINTS[0][1]);
         //draw a line from one point to the next
-        for(int index = 1; index < points.length; index++) {
-            segment.lineTo(points[index][0], points[index][1]);
+        for(int index = 1; index < POINTS.length; index++) {
+            segment.lineTo(POINTS[index][0], POINTS[index][1]);
         }
         //close off the line to create a shape
         segment.closePath();
@@ -91,13 +91,13 @@ public class LEDPanel extends Panel {
     }
 
     //make number filters constants
-    private static final int[] topSegmentFilter         = {0, 2, 3, 5, 6, 7, 8, 9};
-    private static final int[] topLeftSegmentFilter     = {0, 4, 5, 6, 8, 9};
-    private static final int[] topRightSegmentFilter    = {0, 1, 2, 3, 4, 7, 8, 9};
-    private static final int[] middleSegmentFilter      = {2, 3, 4, 5, 6, 8, 9};
-    private static final int[] bottomLeftSegmentFilter  = {0, 2, 6, 8};
-    private static final int[] bottomRightSegmentFilter = {0, 1, 3, 4, 5, 6, 7, 8, 9};
-    private static final int[] bottomSegmentFilter      = {0, 2, 3, 5, 6, 8};
+    private static final int[] TOP_SEGMENT_FILTER = {0, 2, 3, 5, 6, 7, 8, 9};
+    private static final int[] TOP_LEFT_SEGMENT_FILTER = {0, 4, 5, 6, 8, 9};
+    private static final int[] TOP_RIGHT_SEGMENT_FILTER = {0, 1, 2, 3, 4, 7, 8, 9};
+    private static final int[] MIDDLE_SEGMENT_FILTER = {-1, 2, 3, 4, 5, 6, 8, 9};
+    private static final int[] BOTTOM_LEFT_SEGMENT_FILTER = {0, 2, 6, 8};
+    private static final int[] BOTTOM_RIGHT_SEGMENT_FILTER = {0, 1, 3, 4, 5, 6, 7, 8, 9};
+    private static final int[] BOTTOM_SEGMENT_FILTER = {0, 2, 3, 5, 6, 8};
 
     public void paint(Graphics g) {
         //convert (up-cast) the Graphics object to
@@ -121,49 +121,49 @@ public class LEDPanel extends Panel {
 
         //create the segments.
         //See documentation for calculations
-        //of origin points
+        //of origin POINTS
 
         //top segment
         populateSegmentPoints(segmentThickness,
                               0,
                               segmentLength,
                               segmentThickness);
-        displaySegment(g2, topSegmentFilter);
+        displaySegment(g2, TOP_SEGMENT_FILTER);
         //top left segment
         populateSegmentPoints(0,
                               segmentThickness,
                               segmentThickness,
                               segmentLength);
-        displaySegment(g2, topLeftSegmentFilter);
+        displaySegment(g2, TOP_LEFT_SEGMENT_FILTER);
         //top right segment
         populateSegmentPoints(segmentLength + segmentThickness,
                               segmentThickness,
                               segmentThickness,
                               segmentLength);
-        displaySegment(g2, topRightSegmentFilter);
+        displaySegment(g2, TOP_RIGHT_SEGMENT_FILTER);
         //middle segment
         populateSegmentPoints(segmentThickness,
                               segmentLength + segmentThickness,
                               segmentLength,
                               segmentThickness);
-        displaySegment(g2, middleSegmentFilter);
+        displaySegment(g2, MIDDLE_SEGMENT_FILTER);
         //bottom left segment
         populateSegmentPoints(0,
                               segmentThickness * 2 + segmentLength,
                               segmentThickness,
                               segmentLength);
-        displaySegment(g2, bottomLeftSegmentFilter);
+        displaySegment(g2, BOTTOM_LEFT_SEGMENT_FILTER);
         //bottom right segment
         populateSegmentPoints(segmentLength + segmentThickness,
                               segmentThickness * 2 + segmentLength,
                               segmentThickness,
                               segmentLength);
-        displaySegment(g2, bottomRightSegmentFilter);
+        displaySegment(g2, BOTTOM_RIGHT_SEGMENT_FILTER);
         //bottom segment
         populateSegmentPoints(segmentThickness,
                               2 * (segmentLength + segmentThickness),
                               segmentLength,
                               segmentThickness);
-        displaySegment(g2, bottomSegmentFilter);
+        displaySegment(g2, BOTTOM_SEGMENT_FILTER);
     }
 }
